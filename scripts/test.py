39c9e234fe749552a4e46204e00fd6219c14fbb7
lethienquan28052006 <lethienquan28052006@gmail.com>
2025-03-13 12:33:05 +0700

    doi ten 0 1 2 3 -> linh chi dui ga ...

--- scripts/test.py
+++ scripts/test.py
@@ -14,6 +14,13 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
+label_to_mushroom = {
+    0: "Bào ngư",
+    1: "Đùi gà",
+    2: "Linh chi",
+    3: "Nấm mỡ"
+}
+
 # Test ảnh từ thư mục test_data/
 import os
 test_dir = "test_data"
@@ -25,4 +32,6 @@ for img_name in os.listdir(test_dir):
     output = model(image)
     pred = torch.argmax(output, dim=1).item()
     
-    print(f"Ảnh {img_name} được dự đoán là loại nấm: {pred}")
+    predicted_mushroom = label_to_mushroom[pred]
+    
+    print(f"Ảnh {img_name} được dự đoán là loại nấm: {predicted_mushroom}")
